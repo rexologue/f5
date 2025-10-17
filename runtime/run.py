f44c4bf6d5069adcaca9b932193493ab4fb6b17f
rexologue <108322573+rexologue@users.noreply.github.com>
2025-10-17 23:51:20 +0300

    Handle invalid CUDA device selection

--- runtime/run.py
+++ runtime/run.py
@@ -1,13 +1,12 @@
 from pathlib import Path
 from f5tts_trt import F5TTSTRT
 
-f5tts = F5TTSTRT(  
+f5tts = F5TTSTRT(
     config_path=Path("/home/user5/f5/model.yaml"),
     vocab_file=Path("/home/user5/f5_model/vocab.txt"),
     ckpt_file=Path("/home/user5/f5_model/espeech_tts_rlv2.pt"),
-    trt_dit_dir=Path("/home/user5/f5_new_trt/trt_f5_engine"),        
-    trt_vocoder_dir=Path("/home/user5/f5_new_trt/vocoder_engine.plan"),    
-    device="cuda:5",
+    trt_dit_dir=Path("/home/user5/f5_new_trt/trt_f5_engine"),
+    trt_vocoder_dir=Path("/home/user5/f5_new_trt/vocoder_engine.plan"),
 )
 
 wav, sr, spec = f5tts.infer(
